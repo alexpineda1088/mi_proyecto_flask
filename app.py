6f6c09751eb744bb686420b37a52bfa9e3331668
Alex Pineda <159101357+alexpineda1088@users.noreply.github.com>
2025-08-17 22:26:26 -0500

    mi_proyecto_flask

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return "¡Hola, bienvenido a mi proyecto Flask, Alex!"
+
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f"Bienvenido, {nombre}!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
